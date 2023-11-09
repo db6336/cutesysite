595da2668843901f34b845484be9d41734a7e3cf
Aroga <33468690+db6336@users.noreply.github.com>
2023-11-09 00:24:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-let loveBox = $(".container").hide();
+let loveBox = $(".container");
+loveBox.hide();
+
+$("title").text("who dis");
 
 password = prompt("Hold your horses! Who is my Special Guy >:(").toLowerCase();
 
 if (password == "darkphantomlord" || password == "phantomlord" || password == "darkphantom") {
     $("body").addClass("background-romantic");
+    $("title").text("DarkPhantomLord 💌);
     loveBox.show();
 } else if (password !== "darkphantomlord") {
+    $("title").text("Nah fam wtf is dis");
     $("h3").text("Well that's awkward...")
     $("p").html("You're not my <strong>DPL!</strong> Get outta hereeee")
     loveBox.show();
